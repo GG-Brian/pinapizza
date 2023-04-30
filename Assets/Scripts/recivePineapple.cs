eec71159ac8879aaaaa794ccc5692b0de8737486
Adrián Lorenzo Barrios <adrianlorenzbarrios@gmail.com>
2023-04-30 17:54:38 +0200

    receivePineapple fixed

--- Assets/Scripts/recivePineapple.cs
+++ Assets/Scripts/recivePineapple.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class recivePineapple : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int index = 0;
+    private static bool touchedFirstTime = false;
     void Start()
     {
         
@@ -16,5 +17,25 @@ public class recivePineapple : MonoBehaviour
         
     }
 
-  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && hud.municion < 9)
+        {
+            if (!touchedFirstTime)
+            {
+                touchedFirstTime = true;
+                index++;
+                hud.municion += 5;
+                Debug.Log("lol");
+            }
+            if (index == 0)
+            {
+                hud.municion += 2;
+                index++;
+            }
+            //hud.score++;
+        }
+    }
+
+
 }
