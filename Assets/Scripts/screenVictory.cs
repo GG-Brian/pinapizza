0801fc941bceb31dabb58647a8dc5ca34004363c
GG-Brian <71889035+GG-Brian@users.noreply.github.com>
2023-05-05 20:02:39 +0100

    gameplay HUD & Screen sizing fix

--- Assets/Scripts/screenVictory.cs
+++ Assets/Scripts/screenVictory.cs
@@ -7,18 +7,22 @@ using TMPro;
 public class screenVictory : MonoBehaviour {
 
     private Vector3 pointerClick;
-
+    private Scene scene;
     public static int pizzaTotal = 0;
 
 void Awake()
     {
+        Scene scene = SceneManager.GetActiveScene();
         pizzaTotal = 0;
     }
     void Update(){
         if (Input.GetMouseButtonDown(1)) {
             pointerClick = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         }
-         GameObject.Find("PizzaTotal").GetComponent<TextMeshProUGUI>().text = "Pizzas enviadas: " + pizzaTotal;
+        if (scene.name == "Victory") {
+            GameObject.Find("PizzaTotal").GetComponent<TextMeshProUGUI>().text = "Pizzas enviadas: " + pizzaTotal;
+        }
+
     }
 
     public void gameMenu() {
